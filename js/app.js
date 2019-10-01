91786a8ca2d94fd42e250c38e92bde1c63afbdd7
Amy Evans <amykarline@yahoo.com>
2019-10-01 16:47:07 -0700

    first prompt and some css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-alert('I am alive');
+'use strict';
+
+var userName = prompt('What\'s your name?');
+alert('Hi ' + userName + '!');
+
+var whereWasIBorn = prompt('Hi! I\'m Amy, was I born in Washington?');
+whereWasIBorn = whereWasIBorn.toLowerCase();
+
+if(whereWasIBorn === 'yes') {
+  alert('Nope! I was born in Omaha, Nebraska!');
+} else {
+  alert('You\'re right! I was born in Omaha Nebraska!');
+}
+
